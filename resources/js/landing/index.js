b20c9eb9ff84e9b89574cec51057dfc47c25f2f0
QobilovSardor <note47297@gmail.com>
2024-10-06 19:16:49 +0500

    project finished

--- resources/js/landing/index.js
+++ resources/js/landing/index.js
@@ -1,34 +1,4 @@
-
 $(document).ready(function () {
-  // Filter buttonlar uchun click funksiyasi
-  $('.select-btn').on('click', function () {
-    $('.select-btn').removeClass('active'); // Hamma buttonlardan active classini olib tashlash
-    $(this).addClass('active'); // Faqat bosilgan button active bo'ladi
-  });
-});
-$(document).ready(function () {
-  // Boshlang'ich holatda select list yopiq bo'ladi
-  $('.list-works__select-options').slideUp();
-
-  // Select Boxni ochish va yopish uchun
-  $('.selected-box').on('click', function () {
-    if ($('.list-works__select-options').is(':visible')) {
-      // Agar ochiq bo'lsa, uni slideUp yordamida yopamiz
-      $('.list-works__select-options').slideUp();
-    } else {
-      // Agar yopiq bo'lsa, uni slideDown yordamida ochamiz
-      $('.list-works__select-options').slideDown();
-    }
-  });
-
-  // Select option bosilganda textni yangilash
-  $('.list-works__select-option').on('click', function () {
-    let selectedText = $(this).text();
-    $('.selected-text').text(selectedText); // Tanlangan textni yangilash
-    $('.list-works__select-option').removeClass('active');
-    $(this).addClass('active'); // Tanlangan optionni active qilish
-    $('.list-works__select-options').slideUp(); // Select listni yopish
-  });
 
   // Boshida barcha accordionni yopamiz va faqat birinchisini ochamiz
   $('.accordion-content').hide(); // Barcha accordionlarni yopamiz
@@ -52,72 +22,20 @@ $(document).ready(function () {
     }
   });
 
-  // $('.select-number__box').on('click', function () {
-  //   $('.country-select').slideToggle();
-  // })
 
-  // Boshlang'ich holatda dropdownni yopamiz
-  $('.country-select').slideUp();
 
-  // Select dropdownni ochish va yopish (slideToggle orqali)
-  $('.selectext-box').on('click', function () {
-    $('.country-select').slideToggle(); // Dropdownni ochish yoki yopish
-  });
-
-  // Country code tanlanganda yangilash
-  $('.country-code').on('click', function () {
-    var selectedCode = $(this).text(); // Tanlangan kodni olish
-    $('.selected-code').text(selectedCode); // Tanlangan kodni yuqorisida ko'rsatish
-    $('.country-code').removeClass('active'); // Hammasidan active olib tashlash
-    $(this).addClass('active'); // Faqat tanlanganga active qo'shish
-    $('.country-select').slideUp(); // Tanlanganidan so'ng dropdownni yopish
-  });
-
-  // Sahifa tashqarisiga bosganda dropdownni yopish
-  $(document).on('click', function (e) {
-    if (!$(e.target).closest('.select-number__box').length) {
-      $('.country-select').slideUp(); // Dropdown menyuni tashqaridan bosilganda yopish
+  // Checkbox holatini tekshirish
+  $('#check').on('change', function () {
+    if ($(this).is(':checked')) {
+      // Agar checkbox belgilangan bo'lsa, disabled classini olib tashlaymiz va tugmani faollashtiramiz
+      $('.send-btn').removeClass('disabled');
+      $('.send-btn').prop('disabled', false); // Tugmani faollashtirish
+    } else {
+      // Agar checkbox belgilanmagan bo'lsa, disabled classini qo'shamiz va tugmani nofaol qilamiz
+      $('.send-btn').addClass('disabled');
+      $('.send-btn').prop('disabled', true); // Tugmani nofaol qilish
     }
   });
-
-
-  $(document).ready(function () {
-    // Select dropdownni ochish va yopish
-    $('.select-options').slideUp();
-    $('.selected-box-btn').on('click', function () {
-      $('.select-options').slideToggle(); // Dropdown menyuni ochish yoki yopish
-    });
-
-    // Select option bosilganda qiymatni yangilash
-    $('.select-option').on('click', function () {
-      var selectedOption = $(this).text(); // Tanlangan option textini olish
-      $('.selected-box span').text(selectedOption); // Tanlangan textni selected-boxga qo'yish
-      $('.select-options').slideUp(); // Dropdown menyuni yopish
-    });
-
-    // Dropdown tashqarisiga bosilganda menyuni yopish
-    $(document).on('click', function (e) {
-      if (!$(e.target).closest('.select').length) {
-        $('.select-options').slideUp(); // Agar selectdan tashqariga bosilsa, menyuni yopish
-      }
-    });
-  });
-
-  $(document).ready(function () {
-    // Checkbox holatini tekshirish
-    $('#check').on('change', function () {
-      if ($(this).is(':checked')) {
-        // Agar checkbox belgilangan bo'lsa, disabled classini olib tashlaymiz va tugmani faollashtiramiz
-        $('.send-btn').removeClass('disabled');
-        $('.send-btn').prop('disabled', false); // Tugmani faollashtirish
-      } else {
-        // Agar checkbox belgilanmagan bo'lsa, disabled classini qo'shamiz va tugmani nofaol qilamiz
-        $('.send-btn').addClass('disabled');
-        $('.send-btn').prop('disabled', true); // Tugmani nofaol qilish
-      }
-    });
-  });
-
   $('.right-sidebar__show-btn').on('click', function () {
     $('.right-sidebar').toggleClass('show');
   })
@@ -125,5 +43,69 @@ $(document).ready(function () {
     $('.work-pages__list-item').removeClass('active');
     $(this).addClass('active');
   });
-});
 
+  // Dropdown select uchun universal funksiya
+  function initializeCustomSelect(selectBoxSelector) {
+    // Boshlang'ich holatda barcha dropdownlarni yopamiz
+    $(selectBoxSelector + ' .select-options').slideUp();
+
+    // Select dropdownni ochish va yopish
+    $(selectBoxSelector + ' .selected-box').on('click', function () {
+      $(selectBoxSelector + ' .select-options').slideToggle();
+    });
+
+    // Select option bosilganda qiymatni yangilash va menyuni yopish
+    $(selectBoxSelector + ' .select-option').on('click', function () {
+      var selectedOption = $(this).text(); // Tanlangan option textini olish
+      $(selectBoxSelector + ' .selected-box span').text(selectedOption); // Tanlangan textni yangilash
+      $(selectBoxSelector + ' .select-option').removeClass('active'); // Barcha optionlardan 'active' classini olib tashlash
+      $(this).addClass('active'); // Tanlangan optionga 'active' class qo'shish
+      $(selectBoxSelector + ' .select-options').slideUp(); // Dropdown menyuni yopish
+    });
+
+    // Dropdown tashqarisiga bosilganda menyuni yopish
+    $(document).on('click', function (e) {
+      if (!$(e.target).closest(selectBoxSelector).length) {
+        $(selectBoxSelector + ' .select-options').slideUp();
+      }
+    });
+  }
+
+  // Bir nechta select-boxlarni initsializatsiya qilish
+  initializeCustomSelect('.modal-select');
+  initializeCustomSelect('.list-works__select');
+  initializeCustomSelect('.select-number__box');
+
+  $('.select-number__box').on('click', function () {
+    $('.country-select').slideToggle();
+  })
+
+  // // Boshlang'ich holatda dropdownni yopamiz
+  $('.country-select').slideUp();
+
+  // // Country code tanlanganda yangilash
+  $('.country-code').on('click', function () {
+    var selectedCode = $(this).text(); // Tanlangan kodni olish
+    $('.selected-code').text(selectedCode); // Tanlangan kodni yuqorisida ko'rsatish
+    $('.country-code').removeClass('active'); // Hammasidan active olib tashlash
+    // $(this).addClass('active'); // Faqat tanlanganga active qo'shish
+    // $('.country-select').slideUp(); // Tanlanganidan so'ng dropdownni yopish
+  });
+
+  // // Sahifa tashqarisiga bosganda dropdownni yopish
+  if (!$(e.target).closest('.select-number__box').length) {
+    $('.country-select').slideUp(); // Dropdown menyuni tashqaridan bosilganda yopish
+  }
+
+  // Dropdown tashqarisiga bosilganda menyuni yopish
+  // $(document).on('click', function (e) {
+  //   if (!$(e.target).closest('.select').length) {
+  //     $('.select-options').slideUp();
+  //   }
+  // });
+
+  $('.select-btn').on('click', function () {
+    $('.select-btn').removeClass('active'); // Hamma buttonlardan active classini olib tashlash
+    $(this).addClass('active'); // Faqat bosilgan button active bo'ladi
+  });
+});
