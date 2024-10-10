c90840c7ed1c3cb854c50ce6ba5968a42193d3cc
QobilovSardor <note47297@gmail.com>
2024-10-10 13:29:46 +0500

    S

--- resources/js/authorization/authorization.js
+++ resources/js/authorization/authorization.js
@@ -5,3 +5,23 @@ $('.work-pages__list-item').on('click', function () {
   $('.work-pages__list-item').removeClass('active');
   $(this).addClass('active');
 });
+$(document).ready(function () {
+  let stars = $('.stars-box svg');
+
+  // Hover effekti
+  stars.hover(function () {
+    let index = $(this).index(); // Hozirgi yulduz indexini olish
+    // 1-dan hozirgi indexgacha bo'lgan yulduzlarni hover qilish
+    stars.slice(0, index + 1).addClass('hover');
+  }, function () {
+    stars.removeClass('hover'); // Hoverdan olganda hover klasslarini olib tashlash
+  });
+
+  // Click effekti
+  stars.click(function () {
+    let index = $(this).index(); // Hozirgi yulduz indexini olish
+    stars.removeClass('active'); // Barcha yulduzlardan active klassini olib tashlash
+    // 1-dan hozirgi indexgacha bo'lgan yulduzlarni active qilish
+    stars.slice(0, index + 1).addClass('active');
+  });
+});
